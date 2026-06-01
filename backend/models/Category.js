97923b996a4242d63006e09f66b0fab901861caa
keerthana290506 <keerthana290506@gmail.com>
2026-06-01 12:03:51 +0530

    Fix Category model export

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema(
   {
@@ -12,4 +12,4 @@ const categorySchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export default mongoose.model("Category", categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
